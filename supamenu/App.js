232a0c28fa215d89225c7b056fbe9c3ed8768d04
MarthUwir <uwrmaritha250@gmail.com>
2024-05-15 22:10:34 +0200

    my first mobile app

---
+++ supamenu/App.js
@@ -0,0 +1,21 @@
+// App.js
+
+import 'react-native-gesture-handler';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import HomeScreen from './screens/HomeScreen'; // Import your screen components
+// Import other screen components here
+
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeScreen} />
+        {/* Add other screens here */}
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
